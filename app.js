0d45a2113335b6ded53f623c1f72442a2096e045
DarkKevo <kevinaraujogonzalez@gmail.com>
2022-02-24 19:41:53 -0400

    Estructura(HTML-CSS: 90%) Algunas animaciones, Menu  Ham listo

---
+++ app.js
@@ -0,0 +1,67 @@
+$(document).ready(function () {
+	//Drop Automatico
+	$('.barra1').click(function () {
+		$('#li1').slideToggle();
+		$('#li2').slideUp();
+		$('#li3').slideUp();
+		$('#li4').slideUp();
+		$('#li5').slideUp();
+		$('#li6').slideUp();
+		$('#li7').slideUp();
+	});
+    $('.barra2').click(function () {
+		$('#li2').slideToggle();
+		$('#li1').slideUp();
+		$('#li3').slideUp();
+		$('#li4').slideUp();
+		$('#li5').slideUp();
+		$('#li6').slideUp();
+		$('#li7').slideUp();
+	});
+    $('.barra3').click(function () {
+		$('#li3').slideToggle();
+		$('#li1').slideUp();
+		$('#li2').slideUp();
+		$('#li4').slideUp();
+		$('#li5').slideUp();
+		$('#li6').slideUp();
+		$('#li7').slideUp();
+	});
+
+    $('.barra4').click(function () {
+		$('#li4').slideToggle();
+		$('#li1').slideUp();
+		$('#li2').slideUp();
+		$('#li3').slideUp();
+		$('#li5').slideUp();
+		$('#li6').slideUp();
+		$('#li7').slideUp();
+	});
+    $('.barra5').click(function () {
+		$('#li5').slideToggle();
+		$('#li1').slideUp();
+		$('#li2').slideUp();
+		$('#li3').slideUp();
+		$('#li4').slideUp();
+		$('#li6').slideUp();
+		$('#li7').slideUp();
+	});
+    $('.barra6').click(function () {
+		$('#li6').slideToggle();
+		$('#li1').slideUp();
+		$('#li2').slideUp();
+		$('#li3').slideUp();
+		$('#li4').slideUp();
+		$('#li5').slideUp();
+		$('#li7').slideUp();
+	});
+    $('.barra7').click(function () {
+		$('#li7').slideToggle();
+		$('#li1').slideUp();
+		$('#li2').slideUp();
+		$('#li3').slideUp();
+		$('#li4').slideUp();
+		$('#li5').slideUp();
+		$('#li6').slideUp();
+	});
+});
